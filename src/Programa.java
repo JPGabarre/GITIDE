692be5e737ff38ce057d1dcf200b6409dafb702d
juanp_ksx3m2c <juanp_ksx3m2c@DESKTOP-O1H7D3O>
2019-10-11 22:29:08 +0200

    Commit nomes local

--- src/Programa.java
+++ src/Programa.java
@@ -10,6 +10,9 @@ public class Programa {
         for(int i = 0; i <5;i++) {
         	System.out.println(i);
         }
+        for(int i = 0; i <5;i++) {
+        	System.out.println("mariobros");
+        }
 	}
 
 }
