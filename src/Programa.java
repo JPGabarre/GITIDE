75574da9a598fb0717918a0f59219efee2bd44b2
juanp_ksx3m2c <juanp_ksx3m2c@DESKTOP-O1H7D3O>
2019-10-11 20:55:22 +0200

    Modificació del arxiu

--- src/Programa.java
+++ src/Programa.java
@@ -5,6 +5,9 @@ public class Programa {
 		// TODO Auto-generated method stub
 		int a = 0;// Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World "+a);
+        for(int i = 0; i <5;i++) {
+        	System.out.println(i);
+        }
 	}
 
 }
