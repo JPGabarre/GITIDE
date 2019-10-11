e68d7c8177761e7a31f3efe3c3ea24c7c349fb03
juanp_ksx3m2c <juanp_ksx3m2c@DESKTOP-O1H7D3O>
2019-10-11 21:44:38 +0200

    Pujar a remot

--- src/Programa.java
+++ src/Programa.java
@@ -3,8 +3,7 @@ public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 0;// Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World "+a);
+        System.out.println("Hello, World ");
         for(int i = 0; i <5;i++) {
         	System.out.println(i);
         }
