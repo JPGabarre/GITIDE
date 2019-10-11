9b4c73c5c2307a7ea01e5519f878b1da209ce07d
juanp_ksx3m2c <juanp_ksx3m2c@DESKTOP-O1H7D3O>
2019-10-11 21:57:15 +0200

    Intent de crear conflicte

--- src/Programa.java
+++ src/Programa.java
@@ -1,12 +1,10 @@
 
-public class Programa {
 
-	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello, World ");
+        System.out.println("Hello, World bonas tardas");
         for(int i = 0; i <5;i++) {
         	System.out.println(i);
         }
-	}
-
-}
+        for(int i = 0; i <5;i++) {
+        	System.out.println(i+3);
+        }
