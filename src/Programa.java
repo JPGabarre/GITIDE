1e9a48f209d8180296615d2ff2002a721247bd79
juanp_ksx3m2c <juanp_ksx3m2c@DESKTOP-O1H7D3O>
2019-10-11 20:40:20 +0200

    El primer commit

---
+++ src/Programa.java
@@ -0,0 +1,10 @@
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;// Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World "+a);
+	}
+
+}
