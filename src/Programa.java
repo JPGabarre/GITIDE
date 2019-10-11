04d9ed1ecaf1f533dfa8e057cc4ef367ac0d96c1
juanp_ksx3m2c <juanp_ksx3m2c@DESKTOP-O1H7D3O>
2019-10-11 21:58:21 +0200

    Ni idea

--- src/Programa.java
+++ src/Programa.java
@@ -1,10 +1,15 @@
 
+public class Programa {
 
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello, World bonas tardas");
         for(int i = 0; i <5;i++) {
         	System.out.println(i);
         }
         for(int i = 0; i <5;i++) {
-        	System.out.println(i+3);
+        	System.out.println(i);
         }
+	}
+
+}
